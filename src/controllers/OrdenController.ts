a30711f7cefa58c04585aa3dd5e3147f5a813d7f
Bryantgus <bryanttejeda628f@hotmail.com>
2026-02-21 08:01:29 -0400

    Agregando rutas de obtener todas las ordenes y editar orden

--- src/controllers/OrdenController.ts
+++ src/controllers/OrdenController.ts
@@ -3,18 +3,28 @@ import { z } from "zod";
 import { prisma } from "../config/db";
 import { catchAsync } from "../middleware/catchAsync";
 
-const OrdenSchema = z.object({
+const createOrden = z.object({
   cliente: z.string().min(3, "El nombre debe tener al menos 3 caracteres"),
   estado: z.string().refine((val) => ["Pendiente", "Procesando", "Completado"].includes(val), {
     message: "Estado no válido"
   }),
+  descripcion: z.string().optional(),
+  asignadoA: z.string().optional()
 });
+
+const editarOrden = z.object({
+  cliente: z.string().min(3, "El nombre debe tener al menos 3 caracteres").optional(),
+  estado: z.string().refine((val) => ["Pendiente", "Procesando", "Completado"].includes(val), {
+    message: "Estado no válido"
+  }).optional(),
+  descripcion: z.string().optional(),
+  asignadoA: z.string().optional()
+})
 
 export class OrdenController {
   static create = catchAsync(async (req: Request, res: Response) => {
-    const validatedData = OrdenSchema.parse(req.body);
+    const validatedData = createOrden.parse(req.body);
 
-    // 2. Guardar
     const nuevaOrden = await prisma.orden.create({
       data: {
         ...validatedData,
@@ -24,4 +34,27 @@ export class OrdenController {
 
     res.status(201).json(nuevaOrden);
   });
+
+  static getAll = async (req: Request, res: Response) => {
+    const ordenes = await prisma.orden.findMany()
+    res.status(200).json(ordenes);
+  };
+
+  static modify = async (req: Request, res: Response) => {
+    const { id } = req.params
+    console.log("ID recibido:", id);
+    const validatedData = editarOrden.parse(req.body);
+
+    const ordenModificada = await prisma.orden.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        ...validatedData
+      }
+    });
+
+    res.status(201).json(ordenModificada);
+
+  }
 }
--- src/routes/OrdenRouter.ts
+++ src/routes/OrdenRouter.ts
@@ -3,6 +3,7 @@ import { OrdenController } from "../controllers/OrdenController";
 
 const router = Router();
 
-router.get('/', OrdenController.create)
-
-export default router;
+router.post('/', OrdenController.create)
+router.get('/', OrdenController.getAll)
+router.patch('/:id', OrdenController.modify)
+export default router;  
