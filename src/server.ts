c58555f1de23a87eb4b68c23ca2285061a21ba31
Bryantgus <bryanttejeda628f@hotmail.com>
2026-05-05 18:19:07 -0400

    Agregando .env de frontend

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
 import express from 'express'
-import colors from 'colors'
-import { OrdenController } from './controllers/OrdenController'
 import OrdenRouter from './routes/OrdenRouter'
 import { globalErrorHandler } from './middleware/errorHandler'
 import cors from 'cors'
@@ -9,7 +7,7 @@ import CroquisRouter from './routes/CroquisRouter'
 const app = express()
 
 app.use(cors({
-  origin: 'http://localhost:5173', // La URL de tu frontend (Vite por defecto)
+  origin: process.env.CLIENT_URL, // La URL de tu frontend (Vite por defecto)
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true // Por si luego usas cookies o sesiones
 }));
